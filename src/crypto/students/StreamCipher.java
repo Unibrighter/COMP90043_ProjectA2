f3353ec4e078e0299a7bea5fa55cac43fc7abb0f
Unibrighter <rocklee.au@foxmail.com>
2015-09-29 22:13:28 +1000

    supplementary.java finished and passed the personal test

--- src/crypto/students/StreamCipher.java
+++ src/crypto/students/StreamCipher.java
@@ -6,87 +6,118 @@ import java.util.Base64;
 
 import org.apache.log4j.Logger;
 
-public class StreamCipher {
-	
+public class StreamCipher
+{
+
 	private static Logger log = Logger.getLogger(StreamCipher.class);
-	
+
 	private BigInteger key;
 	private BigInteger prime;
 	private BigInteger p1;
 	private BigInteger p2;
 	private BigInteger r_i;
-	
-	public StreamCipher(BigInteger share, BigInteger prime, BigInteger p, BigInteger q) {
+
+	public StreamCipher(BigInteger share, BigInteger prime, BigInteger p,
+			BigInteger q)
+	{
 		this.key = share; // shared key from DH
 		this.prime = prime; // DH prime modulus
 		this.p1 = Supplementary.deriveSuppementaryKey(share, p);
 		this.p2 = Supplementary.deriveSuppementaryKey(share, q);
 		this.r_i = BigInteger.ZERO; // shift register
 	}
-	
+
 	/***
 	 * Updates the shift register for XOR-ing the next byte.
 	 */
-	public void updateShiftRegister() {
-		log.error("You must implement this function!");
+	public void updateShiftRegister()
+	{
+		// r_i=(a x r_(i-1) + b ) mod p;
+
+		// log.error("You must implement this function!");
 	}
 
 	/***
 	 * This function returns the shift register to its initial position
 	 */
-	public void reset() {
-		log.error("You must implement this function!");
+	public void reset()
+	{
+
+		// log.error("You must implement this function!");
 	}
-	
+
 	/***
 	 * Gets N numbers of bits from the MOST SIGNIFICANT BIT (inclusive).
-	 * @param value Source from bits will be extracted
-	 * @param n The number of bits taken
+	 * 
+	 * @param value
+	 *            Source from bits will be extracted
+	 * @param n
+	 *            The number of bits taken
 	 * @return The n most significant bits from value
 	 */
-	private byte msb(BigInteger value, int n) {
-		log.error("You must implement this function!");
-		return (byte)0x00;
+	private byte msb(BigInteger value, int n)
+	{
+
+		String binary_msb = value.toString(2).substring(0, n);
+
+		Byte result = Byte.valueOf(binary_msb, 2);
+		return result.byteValue();
+
+		// log.error("You must implement this function!");
+		// return (byte)0x00;
 	}
-	
+
 	/***
 	 * Takes a cipher text/plain text and decrypts/encrypts it.
-	 * @param msg Either Plain Text or Cipher Text.
+	 * 
+	 * @param msg
+	 *            Either Plain Text or Cipher Text.
 	 * @return If PT, then output is CT and vice-versa.
 	 */
-	private byte[] _crypt(byte[] msg) {
-		log.error("You must implement this function!");
-		return null;
+	private byte[] _crypt(byte[] msg)
+	{
+		// get rid of the Stupid BigIntegeter.ZERO and replace it with
+		// R0=CheckSum(kAB)
+		this.reset();
+
+		 log.error("You must implement this function!");
+		 return null;
 	}
-	
-	//-------------------------------------------------------------------//
-	// Auxiliary functions to perform encryption and decryption          //
-	//-------------------------------------------------------------------//
-	public String encrypt(String msg) {
+
+	// -------------------------------------------------------------------//
+	// Auxiliary functions to perform encryption and decryption //
+	// -------------------------------------------------------------------//
+	public String encrypt(String msg)
+	{
 		// input: plaintext as a string
 		// output: a base64 encoded ciphertext string
 		log.debug("line to encrypt: [" + msg + "]");
 		String result = null;
-		try {
+		try
+		{
 			byte[] asArray = msg.getBytes("UTF-8");
 			result = Base64.getEncoder().encodeToString(_crypt(asArray));
 			log.debug("encrypted text: [" + result + "]");
-		} catch (UnsupportedEncodingException e) {
+		} catch (UnsupportedEncodingException e)
+		{
 			e.printStackTrace();
 		}
 		return result;
 	}
-	
-	public String decrypt(String msg) {
-		// input: a base64 encoded ciphertext string 
+
+	public String decrypt(String msg)
+	{
+		// input: a base64 encoded ciphertext string
 		// output: plaintext as a string
 		log.debug("line to decrypt (base64): [" + msg + "]");
 		String result = null;
-		try {
+		try
+		{
 			byte[] asArray = Base64.getDecoder().decode(msg.getBytes("UTF-8"));
 			result = new String(_crypt(asArray), "UTF-8");
 			log.debug("decrypted text; [" + result + "]");
-		} catch (UnsupportedEncodingException e) {
+		} catch (UnsupportedEncodingException e)
+		{
 			e.printStackTrace();
 		}
 		return result;
--- src/crypto/students/Supplementary.java
+++ src/crypto/students/Supplementary.java
@@ -4,37 +4,110 @@ import java.math.BigInteger;
 
 import org.apache.log4j.Logger;
 
+import com.sun.corba.se.spi.ior.Identifiable;
+
 /***
- * In this class all the candidates must implement the methods
- * related to key derivation. You can create auxiliary functions
- * if you need it, using ONLY Java standard classes.
+ * In this class all the candidates must implement the methods related to key
+ * derivation. You can create auxiliary functions if you need it, using ONLY
+ * Java standard classes.
  * 
  * @author Pablo Serrano
  */
-public class Supplementary {
-	
+public class Supplementary
+{
+
 	private static Logger log = Logger.getLogger(Supplementary.class);
-	
+
 	/***
-	 * Receives a 2048 bits key and applies a word by word XOR
-	 * to yield a 64 bit integer at the end.
+	 * Receives a 2048 bits key and applies a word by word XOR to yield a 64 bit
+	 * integer at the end.
 	 * 
-	 * @param key 2048 bit integer form part A1 DH Key Exchange Protocol
+	 * @param key
+	 *            2048 bit integer form part A1 DH Key Exchange Protocol
 	 * @return A 64 bit integer
 	 */
-	public static BigInteger parityWordChecksum(BigInteger key) {
-		log.error("You must implement this function!");
-		return BigInteger.ZERO;
+	public static BigInteger parityWordChecksum(BigInteger key)
+	{
+		String key_binary_str = key.toString(2);
+		// divide the big integer "key" into binary string chunks
+		//matrix of 64 x 32
+		// 01011101...
+		// 10111100...
+		// ...
+		// 11011110...
+
+		//stuff it to standard 2048 bits
+		while (key_binary_str.length()<2048)
+		{
+			key_binary_str="0"+key_binary_str;
+		}
+		//solve it recursively
+		String result_bianry_string = foldXorTo64Bits(key_binary_str);
+
+		return new BigInteger(result_bianry_string,2);
+		
+		// log.error("You must implement this function!");
+		// return BigInteger.ZERO;
 	}
 
 	/***
 	 * 
-	 * @param key 2048 bit integer form part A1 DH Key Exchange Protocol
-	 * @param p A random 64 bit prime integer
+	 * @param key
+	 *            2048 bit integer form part A1 DH Key Exchange Protocol
+	 * @param p
+	 *            A random 64 bit prime integer
 	 * @return A 64 bit integer for use as a key for a Stream Cipher
 	 */
-	public static BigInteger deriveSuppementaryKey(BigInteger key, BigInteger p) {
-		log.error("You must implement this function!");
-		return BigInteger.ZERO;
+	public static BigInteger deriveSuppementaryKey(BigInteger key, BigInteger p)
+	{
+		return key.mod(p);
+		// log.error("You must implement this function!");
+		// return BigInteger.ZERO;
+	}
+
+	private static String xorBinaryString(String str1, String str2)
+	{
+		StringBuffer str_buf = new StringBuffer("");
+		if (str1.length() != str2.length())
+		{
+			log.error("Two strings lengthes do not match!!!");
+			return "";
+		}
+
+		else
+		{
+			for (int i = 0; i < str1.length(); i++)
+			{
+				char ch1 = str1.charAt(i);
+				char ch2 = str2.charAt(i);
+
+				if (!(ch1 == '0' || ch1 == '1') || !(ch2 == '0' || ch2 == '1'))
+				{
+					log.error("Illegal character!!!");
+					return "";
+				}
+
+				if (ch1 == ch2)
+				{
+					str_buf.append('0');
+				} else
+				{
+					str_buf.append('1');
+				}
+			}
+			return str_buf.toString();
+		}
+	}
+
+	private static String foldXorTo64Bits(String str)
+	{
+		if (str.length() <= 64)
+			return str;
+		else
+		{
+			String left_half = str.substring(0, str.length() / 2 );
+			String right_half = str.substring(str.length() / 2);
+			return xorBinaryString(foldXorTo64Bits(left_half), foldXorTo64Bits(right_half));
+		}
 	}
 }
